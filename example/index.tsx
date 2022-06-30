9d07c42c3bde787afed84ce0f0dfa25e8cfcc5bd
xinxin.yuan <xinxin.yuan@trip.com>
2022-06-30 17:58:49 +0800

    ver2.0

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Modal } from '../dist/index'
+import Modal from '../src/components/Modal';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
